679e5f21ce9835283e49270cfeaba66d57b73392
Reinoptland <reinoptland@gmail.com>
2021-03-07 15:53:29 +0100

    Create Articles Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Articles from "./components/Articles";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import AppHeader from "./components/AppHeader";
@@ -8,10 +9,6 @@ import iconYoutube from "./images/icon-youtube.svg";
 import iconTwitter from "./images/icon-twitter.svg";
 import iconPinterest from "./images/icon-pinterest.svg";
 import iconInstagram from "./images/icon-instagram.svg";
-import currency from "./images/image-currency.jpg";
-import restaurant from "./images/image-restaurant.jpg";
-import plane from "./images/image-plane.jpg";
-import confetti from "./images/image-confetti.jpg";
 
 function App() {
   return (
@@ -19,88 +16,7 @@ function App() {
       <AppHeader />
       <Hero />
       <Features />
-      <section className="articles">
-        <div className="article__content container container--pall">
-          <h2>Latest Articles</h2>
-
-          <div className="article__grid">
-            <a href="/" className="article__item">
-              <div
-                className="article__image"
-                style={{ backgroundImage: `url(${currency})` }}
-              ></div>
-              <div className="article__text">
-                <div className="article__author">By Claire Robinson</div>
-                <div className="article__title">
-                  Receive money in any currency with no fees
-                </div>
-                <div className="article__description">
-                  The world is getting smaller and we’re becoming more mobile.
-                  So why should you be forced to only receive money in a single
-                  …
-                </div>
-              </div>
-            </a>
-
-            <a href="/" className="article__item">
-              <div
-                className="article__image"
-                style={{
-                  backgroundImage: `url(${restaurant})`,
-                }}
-              ></div>
-              <div className="article__text">
-                <div className="article__author">By Wilson Hutton</div>
-                <div className="article__title">
-                  Treat yourself without worrying about money
-                </div>
-                <div className="article__description">
-                  Our simple budgeting feature allows you to separate out your
-                  spending and set realistic limits each month. That means you …
-                </div>
-              </div>
-            </a>
-
-            <a href="/" className="article__item">
-              <div
-                className="article__image"
-                style={{ backgroundImage: `url(${plane})` }}
-              ></div>
-              <div className="article__text">
-                <div className="article__author">By Wilson Hutton</div>
-                <div className="article__title">
-                  Take your Easybank card wherever you go
-                </div>
-                <div className="article__description">
-                  We want you to enjoy your travels. This is why we don’t charge
-                  any fees on purchases while you’re abroad. We’ll even show you
-                  …
-                </div>
-              </div>
-            </a>
-
-            <a href="/" className="article__item">
-              <div
-                className="article__image"
-                style={{
-                  backgroundImage: `url(${confetti})`,
-                }}
-              ></div>
-              <div className="article__text">
-                <div className="article__author">By Claire Robinson</div>
-                <div className="article__title">
-                  Our invite-only Beta accounts are now live!
-                </div>
-                <div className="article__description">
-                  After a lot of hard work by the whole team, we’re excited to
-                  launch our closed beta. It’s easy to request an invite through
-                  the site …
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </section>
+      <Articles />
 
       <footer className="footer">
         <div className="container">
---
+++ src/components/Articles.js
@@ -0,0 +1,94 @@
+import React from "react";
+import currency from "../images/image-currency.jpg";
+import restaurant from "../images/image-restaurant.jpg";
+import plane from "../images/image-plane.jpg";
+import confetti from "../images/image-confetti.jpg";
+
+export default function Articles() {
+  return (
+    <section className="articles">
+      <div className="article__content container container--pall">
+        <h2>Latest Articles</h2>
+
+        <div className="article__grid">
+          <a href="/" className="article__item">
+            <div
+              className="article__image"
+              style={{
+                backgroundImage: `url(${currency})`,
+              }}
+            ></div>
+            <div className="article__text">
+              <div className="article__author">By Claire Robinson</div>
+              <div className="article__title">
+                Receive money in any currency with no fees
+              </div>
+              <div className="article__description">
+                The world is getting smaller and we’re becoming more mobile. So
+                why should you be forced to only receive money in a single …
+              </div>
+            </div>
+          </a>
+
+          <a href="/" className="article__item">
+            <div
+              className="article__image"
+              style={{
+                backgroundImage: `url(${restaurant})`,
+              }}
+            ></div>
+            <div className="article__text">
+              <div className="article__author">By Wilson Hutton</div>
+              <div className="article__title">
+                Treat yourself without worrying about money
+              </div>
+              <div className="article__description">
+                Our simple budgeting feature allows you to separate out your
+                spending and set realistic limits each month. That means you …
+              </div>
+            </div>
+          </a>
+
+          <a href="/" className="article__item">
+            <div
+              className="article__image"
+              style={{
+                backgroundImage: `url(${plane})`,
+              }}
+            ></div>
+            <div className="article__text">
+              <div className="article__author">By Wilson Hutton</div>
+              <div className="article__title">
+                Take your Easybank card wherever you go
+              </div>
+              <div className="article__description">
+                We want you to enjoy your travels. This is why we don’t charge
+                any fees on purchases while you’re abroad. We’ll even show you …
+              </div>
+            </div>
+          </a>
+
+          <a href="/" className="article__item">
+            <div
+              className="article__image"
+              style={{
+                backgroundImage: `url(${confetti})`,
+              }}
+            ></div>
+            <div className="article__text">
+              <div className="article__author">By Claire Robinson</div>
+              <div className="article__title">
+                Our invite-only Beta accounts are now live!
+              </div>
+              <div className="article__description">
+                After a lot of hard work by the whole team, we’re excited to
+                launch our closed beta. It’s easy to request an invite through
+                the site …
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
